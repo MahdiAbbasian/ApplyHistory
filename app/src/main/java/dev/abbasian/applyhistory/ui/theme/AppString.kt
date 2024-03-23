da8607671882756d5bc5142766d9ce45be2ce650
mahdiabbasian <mam-1371@outlook.com>
2024-03-24 00:54:40 +0330

    add string resources

---
+++ app/src/main/java/dev/abbasian/applyhistory/ui/theme/AppString.kt
@@ -0,0 +1,16 @@
+package dev.abbasian.applyhistory.ui.theme
+object AppStrings {
+    const val appName = "ApplyHistory"
+    const val companyName = "Company Name"
+    const val companyWebsite = "Company Website"
+    const val description = "Description"
+    const val addCompany = "Add company"
+    const val totalApplied = "Total Applications:"
+    const val applyStatus = "Apply Status"
+    const val updateData = "update data"
+    const val editCompany = "edit company"
+    const val deleteCompany = "delete company"
+    const val search = "search"
+
+    val applyStatusOptions = listOf("Applied", "Rejected", "Interview", "Accepted")
+}
