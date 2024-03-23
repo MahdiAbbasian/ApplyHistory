3bfdd141d2c5b5a0f0bae5dcf723e57d3bad7ddd
mahdiabbasian <mam-1371@outlook.com>
2024-03-24 00:50:11 +0330

    add colors

--- app/src/main/java/dev/abbasian/applyhistory/ui/theme/Color.kt
+++ app/src/main/java/dev/abbasian/applyhistory/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package dev.abbasian.applyhistory.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Teal700 = Color(0xFF018786)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Black2A = Color(0xFF2A2A2A)
+val Divider = Color(0xFFD9D9DB)
+val WhiteSmoke = Color(0xFFF5F5F5)
+val AppliedColor = Color(0xFFFFE600)
+val RejectedColor = Color(0xFFE80B0B)
+val InterviewColor = Color(0xFF2196F3)
+val AcceptedColor = Color(0xFF1CF325)
+val LightGray4 = Color(0xFFEFEFEF)
+val LightGray3 = Color(0xFFE1E1E1)
