8b943d504d817e273a27e59c40b8a711862691b2
mahdiabbasian <mam-1371@outlook.com>
2024-03-24 00:51:31 +0330

    init Application class with koin

---
+++ app/src/main/java/dev/abbasian/applyhistory/App.kt
@@ -0,0 +1,18 @@
+package dev.abbasian.applyhistory
+
+import android.app.Application
+import dev.abbasian.applyhistory.domain.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
