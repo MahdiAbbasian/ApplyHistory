33926ead9874f142186ff69ad8c64d5b7453be45
mahdiabbasian <mam-1371@outlook.com>
2024-03-31 19:12:06 +0330

    fix deep link navigate issue

--- app/src/main/java/dev/abbasian/applyhistory/Route.kt
+++ app/src/main/java/dev/abbasian/applyhistory/Route.kt
@@ -4,5 +4,13 @@ sealed class Route(
     val route: String,
 ){
     object HomeScreen: Route("home_screen")
-    object AddCompanyScreen: Route("add_company_screen")
+    object AddCompanyScreen : Route("add_edit_company_screen")
+
+    object EditCompanyScreen : Route("add_edit_company_screen/{companyId}") {
+        fun createRoute(companyId: Int): String = "add_edit_company_screen/$companyId"
+    }
+
+    object CompanyDetailScreen : Route("companyDetail/{companyId}") {
+        fun createRoute(companyId: Int) = "companyDetail/$companyId"
+    }
 }
