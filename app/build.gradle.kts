2772382a2592b8ffad3c4ffbaf663dbf412c2ce8
mahdiabbasian <mam-1371@outlook.com>
2024-05-23 19:01:15 +0330

    add system ui dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.3.1")
     implementation("androidx.navigation:navigation-compose:2.4.0-beta01")
     implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
