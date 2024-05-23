b6fb1631e862f46beebd5dd3f82628e0ccefec99
mahdiabbasian <mam-1371@outlook.com>
2024-05-23 19:03:05 +0330

    init custom text field

---
+++ app/src/main/java/dev/abbasian/applyhistory/ui/component/CustomTextField.kt
@@ -0,0 +1,135 @@
+package dev.abbasian.applyhistory.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import dev.abbasian.applyhistory.core.extension.UiText
+import dev.abbasian.applyhistory.core.extension.isNumber
+import dev.abbasian.applyhistory.ui.theme.silver
+
+
+@Composable
+fun CustomTextField(
+    placeholder: String,
+    text: String = "",
+    onValueChange: (String) -> Unit = {},
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
+    modifier: Modifier,
+    errorMessage: UiText? = null,
+    isError: Boolean = false,
+    isVisible: Boolean = false,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    singleLine: Boolean = false,
+    maxLine: Int = 1,
+) {
+    val isKeyboardTypeNumber =
+        keyboardType == KeyboardType.Phone || keyboardType == KeyboardType.Number
+    val context = LocalContext.current
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val focusRequester = remember {
+        FocusRequester()
+    }
+    val colorBorder = if (isError) MaterialTheme.colorScheme.error else if (isFocused)
+        MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.primary.copy(alpha = 0.3f)
+
+    Column {
+        BasicTextField(
+            value = if (isKeyboardTypeNumber) {
+                if (isNumber(text)) text else ""
+            } else text,
+            onValueChange = {
+                if (isKeyboardTypeNumber) {
+                    if (isNumber(it)) onValueChange(it)
+                } else onValueChange(it)
+            },
+            textStyle = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.onSurface),
+            maxLines = maxLine,
+            singleLine = singleLine,
+            interactionSource = interactionSource,
+            visualTransformation =
+            if (keyboardType == KeyboardType.Password) {
+                if (isVisible) VisualTransformation.None else PasswordVisualTransformation()
+            } else {
+                VisualTransformation.None
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            decorationBox = { innerTextField ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = modifier
+                        .border(
+                            width = 1.dp,
+                            shape = RoundedCornerShape(8.dp),
+                            color = colorBorder
+                        )
+                        .background(
+                            color = MaterialTheme.colorScheme.surface,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .focusRequester(focusRequester)
+                ) {
+                    if (leadingIcon != null) {
+                        leadingIcon()
+                    } else {
+                        Spacer(modifier = Modifier.padding(8.dp))
+                    }
+                    Box(
+                        modifier = Modifier
+                            .weight(1.0f)
+                            .padding(vertical = 16.dp)
+                    ) {
+                        if (text.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                style = MaterialTheme.typography.bodySmall,
+                                color = silver,
+                            )
+                        }
+                        Box(modifier = Modifier.fillMaxWidth()) {
+                            innerTextField()
+                        }
+                    }
+                    if (trailingIcon != null) {
+                        trailingIcon()
+                    } else {
+                        Spacer(modifier = Modifier.padding(8.dp))
+                    }
+                }
+            },
+        )
+        Text(
+            text = if (isError) errorMessage!!.asString(context) else "",
+            color = MaterialTheme.colorScheme.error,
+            style = MaterialTheme.typography.bodySmall,
+            modifier = modifier
+        )
+    }
+}
