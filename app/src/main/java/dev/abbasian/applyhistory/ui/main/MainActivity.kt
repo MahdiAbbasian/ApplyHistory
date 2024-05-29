f0a7974828f863e30e8dd7eaa45668e7eda21b58
Mahdi Abbasian <mam-1371@outlook.com>
2024-05-30 03:01:10 +0330

    set apply history theme in content

--- app/src/main/java/dev/abbasian/applyhistory/ui/main/MainActivity.kt
+++ app/src/main/java/dev/abbasian/applyhistory/ui/main/MainActivity.kt
@@ -17,6 +17,7 @@ import dev.abbasian.applyhistory.ui.company.edit.EditCompanyScreen
 import dev.abbasian.applyhistory.ui.company.detail.CompanyDetailScreen
 import dev.abbasian.applyhistory.ui.company.CompanyViewModel
 import dev.abbasian.applyhistory.ui.home.HomeScreen
+import dev.abbasian.applyhistory.ui.theme.ApplyHistoryTheme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
@@ -26,40 +27,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            val viewModel: CompanyViewModel = getViewModel()
-            NavHost(navController = navController, startDestination = Route.HomeScreen.route) {
-                composable(route = Route.HomeScreen.route) {
-                    HomeScreen(navController, viewModel)
-                }
-                composable(route = Route.AddCompanyScreen.route) {
-                    EditCompanyScreen(navController, viewModel, null)
-                }
-                composable(
-                    route = Route.EditCompanyScreen.route,
-                    arguments = listOf(navArgument("companyId") {
-                        type = NavType.IntType
-                    })
-                ) {
-                    val companyId = it.arguments?.getInt("companyId") ?: -1
-
-                    LaunchedEffect(companyId) {
-                        if (companyId != -1) {
-                            viewModel.getCompany(companyId)
-                        }
+            ApplyHistoryTheme {
+                val navController = rememberNavController()
+                val viewModel: CompanyViewModel = getViewModel()
+                NavHost(navController = navController, startDestination = Route.HomeScreen.route) {
+                    composable(route = Route.HomeScreen.route) {
+                        HomeScreen(navController, viewModel)
                     }
+                    composable(route = Route.AddCompanyScreen.route) {
+                        EditCompanyScreen(navController, viewModel, null)
+                    }
+                    composable(
+                        route = Route.EditCompanyScreen.route,
+                        arguments = listOf(navArgument("companyId") {
+                            type = NavType.IntType
+                        })
+                    ) {
+                        val companyId = it.arguments?.getInt("companyId") ?: -1
 
-                    val company by viewModel.company.observeAsState()
+                        LaunchedEffect(companyId) {
+                            if (companyId != -1) {
+                                viewModel.getCompany(companyId)
+                            }
+                        }
 
-                    EditCompanyScreen(navController, viewModel, company)
-                }
-                composable(
-                    route = Route.CompanyDetailScreen.route,
-                    arguments = listOf(navArgument("companyId") { type = NavType.IntType })
-                ) {
-                    val companyId = it.arguments?.getInt("companyId")
-                    companyId?.let {
-                        CompanyDetailScreen(navController = navController, viewModel = viewModel, companyId = it)
+                        val company by viewModel.company.observeAsState()
+
+                        EditCompanyScreen(navController, viewModel, company)
+                    }
+                    composable(
+                        route = Route.CompanyDetailScreen.route,
+                        arguments = listOf(navArgument("companyId") { type = NavType.IntType })
+                    ) {
+                        val companyId = it.arguments?.getInt("companyId")
+                        companyId?.let {
+                            CompanyDetailScreen(navController = navController, viewModel = viewModel, companyId = it)
+                        }
                     }
                 }
             }
