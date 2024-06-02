2e6b527f65206d7a99f8fa9583e11aaf2fe954b6
Mahdi Abbasian <mam-1371@outlook.com>
2024-06-02 19:02:36 +0330

    add strings

--- app/src/main/java/dev/abbasian/applyhistory/ui/theme/AppString.kt
+++ app/src/main/java/dev/abbasian/applyhistory/ui/theme/AppString.kt
@@ -1,16 +1,20 @@
 package dev.abbasian.applyhistory.ui.theme
-object AppStrings {
-    const val appName = "ApplyHistory"
-    const val companyName = "Company Name"
-    const val companyWebsite = "Company Website"
-    const val description = "Description"
-    const val addCompany = "Add company"
-    const val totalApplied = "Total Applications:"
-    const val applyStatus = "Apply Status"
-    const val updateData = "update data"
-    const val editCompany = "edit company"
-    const val deleteCompany = "delete company"
-    const val search = "search"
+object AppString {
+    const val SEARCH = "search"
+    const val EXPORT_TO_FILE = "Export to file"
+    const val IMPORT_FROM_FILE = "Import from file"
+    const val IMPORT_FILE_SUCCESS = "Import file Success"
+    const val IMPORT_FILE_FAILED = "Import file Failed"
+    const val TOTAL_APPLICATIONS = "Total Applications:"
+    const val COMPANY_NAME = "Company name"
+    const val COMPANY_WEBSITE = "Company website"
+    const val DESCRIPTION = "Description"
+    const val ADD_COMPANY = "Add company"
+    const val UPDATE_DATA = "Update data"
+    const val APPLY_STATUS = "Apply status"
+    const val EDIT_COMPANY = "Edit company"
+    const val DELETE_COMPANY = "Delete company"
+    const val COMPANY_NOT_FOUND = "Company not found"
 
-    val applyStatusOptions = listOf("Applied", "Rejected", "Interview", "Accepted")
+    val statusOptions = listOf("None", "Applied", "Rejected", "Interview", "Accepted")
 }
